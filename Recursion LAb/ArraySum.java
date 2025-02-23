126ec4c484959d099b74ebe764125e05d71f917e
Drewnicorn15 <67112695+Drewnicorn15@users.noreply.github.com>
2025-02-23 17:32:42 -0500

    Update ArraySum.java

--- Recursion LAb/ArraySum.java
+++ Recursion LAb/ArraySum.java
@@ -2,7 +2,7 @@ public class ArraySum
 {
     public int sumOfArray(Integer[] a,int index )
     {
-        if(index >= a.length|| a[index] == null){return 0;}
-        return a[index]+sumOfArray(a, index+1); 
+       if(index == -1){return 0;}
+        return a[index]+sumOfArray(a, index-1); 
     }
 }
